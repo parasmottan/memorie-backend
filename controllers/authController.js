111cd661165f87f1c14a6ef963e94704b1780e20
paras <parasmottan9316@gmail.com>
2025-08-02 18:38:49 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,9 +57,9 @@ export const login = async (req, res) => {
 
   res
   .cookie("token", token, {
-    httpOnly: false,
-    secure: false,
-    sameSite: "Lax",
+     httpOnly: true,
+  secure: true,
+  sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   })
   .status(200)
